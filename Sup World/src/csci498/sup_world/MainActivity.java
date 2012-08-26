b53cc44ae28a8ef550d7426ddab55cc9d07fc43b
Steven Ruppert <blendmaster@gmail.com>
2012-08-25 21:08:37 -0600

    initial commit

---
+++ Sup World/src/csci498/sup_world/MainActivity.java
@@ -0,0 +1,21 @@
+package csci498.sup_world;
+
+import edu.mines.sup_world.R;
+import android.os.Bundle;
+import android.app.Activity;
+import android.view.Menu;
+
+public class MainActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.activity_main, menu);
+        return true;
+    }
+}
