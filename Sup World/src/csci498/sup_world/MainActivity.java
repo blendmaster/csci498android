d5d6293dc53008fe41fb1f78868ccf01918d4d60
Steven Ruppert <blendmaster@gmail.com>
2012-08-25 21:49:03 -0600

    Replace EditView with big date button
    oh so cool

--- Sup World/src/csci498/sup_world/MainActivity.java
+++ Sup World/src/csci498/sup_world/MainActivity.java
@@ -1,16 +1,31 @@
 package csci498.sup_world;
 
+import java.util.Date;
+
 import edu.mines.sup_world.R;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class MainActivity extends Activity {
+	Button button;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        
+        button = new Button(this);
+        button.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				button.setText(new Date().toString());
+			}
+        });
+        button.setText(new Date().toString());
+        setContentView(button);
+        //setContentView(R.layout.activity_main);
     }
 
     @Override
