da3868ac8162779cc6110d083165df7ff6131222
Steven Ruppert <blendmaster@gmail.com>
2012-08-30 23:49:50 -0600

    Implement Save button functionality

--- MunchList/src/csci498/strupper/munchlist/MunchList.java
+++ MunchList/src/csci498/strupper/munchlist/MunchList.java
@@ -3,13 +3,33 @@ package csci498.strupper.munchlist;
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MunchList extends Activity {
 
+	Restaurant r = new Restaurant();
+	
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_munch_list);
+        
+        Button save = (Button) findViewById(R.id.save);
+        save.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				r.setName(
+						((EditText) findViewById(R.id.name))
+						.getText().toString()
+				);
+				r.setAddress(
+						((EditText) findViewById(R.id.address))
+						.getText().toString()
+				);
+			}
+		});
     }
 
     @Override
