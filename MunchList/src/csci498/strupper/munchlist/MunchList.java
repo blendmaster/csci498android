72b5fe35a8d7b58535e84fad11c0460a0d8acf85
Steven Ruppert <blendmaster@gmail.com>
2012-09-05 13:17:38 -0600

    Normalize code formatting
    Eclipse's code style tool is actually pretty nice. If only it wasn't
    so buried in the menus (along with the "Remove trailing whitespace")
    option. Then maybe more people would use it.
    
    Gotta see if there's an autolint for eclipse too. I'm sure there is.

--- MunchList/src/csci498/strupper/munchlist/MunchList.java
+++ MunchList/src/csci498/strupper/munchlist/MunchList.java
@@ -3,8 +3,8 @@ package csci498.strupper.munchlist;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -16,51 +16,49 @@ import android.widget.RadioGroup;
 
 public class MunchList extends Activity {
 
-	List<Restaurant> restaurants = new ArrayList<Restaurant>();
-	ArrayAdapter<Restaurant> adapter;
+  List<Restaurant> restaurants = new ArrayList<Restaurant>();
+  ArrayAdapter<Restaurant> adapter;
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_munch_list);
-		
-		adapter = new ArrayAdapter<Restaurant>(
-					this, 
-		            android.R.layout.simple_list_item_1,
-		            restaurants);
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_munch_list);
 
-		Button save = (Button)findViewById(R.id.save);
-		save.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				Restaurant r = new Restaurant();
-				r.setName(((EditText) findViewById(R.id.name)).getText()
-						.toString());
-				r.setAddress(((EditText) findViewById(R.id.address)).getText()
-						.toString());
-				
-				RadioGroup types = (RadioGroup) findViewById(R.id.types);
-				switch (types.getCheckedRadioButtonId()) {
-				case R.id.sit_down:
-					r.setType("sit_down");
-					break;
-				case R.id.take_out:
-					r.setType("take_out");
-					break;
-				case R.id.delivery:
-					r.setType("delivery");
-					break;
-				}
+    adapter = new ArrayAdapter<Restaurant>(this,
+                                           android.R.layout.simple_list_item_1,
+                                           restaurants);
 
-				adapter.add(r);
-			}
-		});
+    Button save = (Button)findViewById(R.id.save);
+    save.setOnClickListener(new OnClickListener() {
+      public void onClick(View v) {
+        Restaurant r = new Restaurant();
+        r.setName(((EditText)findViewById(R.id.name)).getText().toString());
+        r.setAddress(((EditText)findViewById(R.id.address)).getText()
+                                                           .toString());
 
-		((ListView)findViewById(R.id.restaurants)).setAdapter(adapter);
-	}
+        RadioGroup types = (RadioGroup)findViewById(R.id.types);
+        switch (types.getCheckedRadioButtonId()) {
+        case R.id.sit_down:
+          r.setType("sit_down");
+          break;
+        case R.id.take_out:
+          r.setType("take_out");
+          break;
+        case R.id.delivery:
+          r.setType("delivery");
+          break;
+        }
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.activity_munch_list, menu);
-		return true;
-	}
+        adapter.add(r);
+      }
+    });
+
+    ((ListView)findViewById(R.id.restaurants)).setAdapter(adapter);
+  }
+
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_munch_list, menu);
+    return true;
+  }
 }
--- Sup World/src/csci498/sup_world/MainActivity.java
+++ Sup World/src/csci498/sup_world/MainActivity.java
@@ -2,35 +2,37 @@ package csci498.sup_world;
 
 import java.util.Date;
 
-import edu.mines.sup_world.R;
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import edu.mines.sup_world.R;
 
 public class MainActivity extends Activity {
-	Button button;
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        button = new Button(this);
-        button.setOnClickListener(new OnClickListener() {
-			public void onClick(View v) {
-				button.setText(new Date().toString());
-			}
-        });
+  Button button;
+
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+
+    button = new Button(this);
+    button.setOnClickListener(new OnClickListener() {
+
+      public void onClick(View v) {
         button.setText(new Date().toString());
-        setContentView(button);
-        //setContentView(R.layout.activity_main);
-    }
+      }
+    });
+    button.setText(new Date().toString());
+    setContentView(button);
+    // setContentView(R.layout.activity_main);
+  }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
+  @Override
+  public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.activity_main, menu);
+    return true;
+  }
 }
