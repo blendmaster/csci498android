e84dc35a09227566652e10465ee12a30b19a70d2
Steven Ruppert <blendmaster@gmail.com>
2012-10-31 22:13:50 -0600

    Wire up mapping activity

--- MunchList/src/csci498/strupper/munchlist/RestaurantMap.java
+++ MunchList/src/csci498/strupper/munchlist/RestaurantMap.java
@@ -1,24 +1,78 @@
 package csci498.strupper.munchlist;
 
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.widget.Toast;
 
+import com.google.android.maps.GeoPoint;
+import com.google.android.maps.ItemizedOverlay;
 import com.google.android.maps.MapActivity;
+import com.google.android.maps.MapView;
+import com.google.android.maps.OverlayItem;
 
 public class RestaurantMap extends MapActivity {
   public static final String EXTRA_NAME = "munchlist.EXTRA_NAME";
   public static final String EXTRA_LON = "munchlist.EXTRA_LON";
   public static final String EXTRA_LAT = "munchlist.EXTRA_LAT";
+  private MapView map;
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.map);
+
+    double lat = getIntent().getDoubleExtra(EXTRA_LAT, 0);
+    double lon = getIntent().getDoubleExtra(EXTRA_LON, 0);
+    map = (MapView)findViewById(R.id.map);
+    map.getController().setZoom(17);
+    GeoPoint status = new GeoPoint((int)(lat * 1000000.0),
+                                   (int)(lon * 1000000.0));
+    map.getController().setCenter(status);
+    map.setBuiltInZoomControls(true);
+    Drawable marker = getResources().getDrawable(R.drawable.marker);
+    marker.setBounds(0, 0, marker.getIntrinsicWidth(),
+                     marker.getIntrinsicHeight());
+    map
+       .getOverlays()
+       .add(new RestaurantOverlay(marker, status,
+                                  getIntent().getStringExtra(EXTRA_NAME)));
+
   }
 
   @Override
   protected boolean isRouteDisplayed() {
     // TODO Auto-generated method stub
     return false;
+  }
+
+  private class RestaurantOverlay extends ItemizedOverlay<OverlayItem> {
+    private OverlayItem item = null;
+
+    public RestaurantOverlay(Drawable marker, GeoPoint point,
+                             String name) {
+      super(marker);
+      boundCenterBottom(marker);
+      item = new OverlayItem(point, name, name);
+      populate();
+    }
+
+    @Override
+    protected OverlayItem createItem(int i) {
+      return (item);
+    }
+
+    @Override
+    public int size() {
+      return (1);
+    }
+
+    @Override
+    protected boolean onTap(int i) {
+      Toast.makeText(RestaurantMap.this,
+                     item.getSnippet(),
+                     Toast.LENGTH_SHORT).show();
+      return true;
+    }
   }
 
 }
