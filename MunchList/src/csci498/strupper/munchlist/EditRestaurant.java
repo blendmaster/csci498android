08e071c828f2544b2fc736a2e0de32336a12d311
Steven Ruppert <blendmaster@gmail.com>
2012-10-31 21:12:21 -0600

    Add GPS glue code to EditRestaurant

--- MunchList/src/csci498/strupper/munchlist/EditRestaurant.java
+++ MunchList/src/csci498/strupper/munchlist/EditRestaurant.java
@@ -3,6 +3,9 @@ package csci498.strupper.munchlist;
 import android.app.Activity;
 import android.content.Intent;
 import android.database.Cursor;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.net.ConnectivityManager;
 import android.os.Bundle;
 import android.view.Menu;
@@ -16,6 +19,32 @@ public class EditRestaurant extends Activity {
 
   private RestaurantHelper helper;
   private String restaurantId;
+  private final LocationListener onLocationChange = new LocationListener() {
+    public void onLocationChanged(Location fix) {
+      helper.updateLocation(restaurantId, fix.getLatitude(),
+                            fix.getLongitude());
+      ((EditText)findViewById(R.id.location)).setText(String.valueOf(fix.getLatitude())
+          + ", "
+          + String.valueOf(fix.getLongitude()));
+      ((LocationManager)getSystemService(LOCATION_SERVICE)).removeUpdates(onLocationChange);
+      Toast
+           .makeText(EditRestaurant.this, "Location saved",
+                     Toast.LENGTH_LONG)
+           .show();
+    }
+
+    public void onProviderDisabled(String provider) {
+      // required for interface, not used
+    }
+
+    public void onProviderEnabled(String provider) {
+      // required for interface, not used
+    }
+
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+      // required for interface, not used
+    }
+  };
 
   @Override
   public void onCreate(Bundle savedInstanceState) {
@@ -57,8 +86,18 @@ public class EditRestaurant extends Activity {
     }
   }
 
+  @Override
+  public boolean onPrepareOptionsMenu(Menu menu) {
+    if (restaurantId == null) {
+      menu.findItem(R.id.location).setEnabled(false);
+    }
+    return super.onPrepareOptionsMenu(menu);
+  }
+
+
   @Override protected void onPause() {
     save();
+    ((LocationManager)getSystemService(LOCATION_SERVICE)).removeUpdates(onLocationChange);
     super.onPause();
   };
 
@@ -140,6 +179,13 @@ public class EditRestaurant extends Activity {
                        Toast.LENGTH_LONG)
              .show();
       }
+      return true;
+    }
+    if (item.getItemId() == R.id.location) {
+      ((LocationManager)getSystemService(LOCATION_SERVICE)).requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                                                                   0,
+                                                                                   0,
+                                                                                   onLocationChange);
       return true;
     }
     return super.onOptionsItemSelected(item);
