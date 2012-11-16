7a19740af41afc05ccd6f3c6ef604dd847a771bb
Steven Ruppert <blendmaster@gmail.com>
2012-11-15 22:09:19 -0700

    Change widget to funky ListWidget
    Didn't keep old widget cause it was boring

--- MunchList/src/csci498/strupper/munchlist/AppWidget.java
+++ MunchList/src/csci498/strupper/munchlist/AppWidget.java
@@ -1,16 +1,37 @@
 package csci498.strupper.munchlist;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
+import android.widget.RemoteViews;
 
 public class AppWidget extends AppWidgetProvider {
   @Override
   public void onUpdate(Context ctxt,
                        AppWidgetManager mgr,
                        int[] appWidgetIds) {
-    ctxt.startService(new Intent(ctxt, WidgetService.class));
+    for (int i = 0; i < appWidgetIds.length; i++) {
+      Intent svcIntent = new Intent(ctxt, WidgetService.class);
+      svcIntent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, appWidgetIds[i]);
+      svcIntent.setData(Uri.parse(svcIntent.toUri(Intent.URI_INTENT_SCHEME)));
+      RemoteViews widget = new RemoteViews(ctxt.getPackageName(),
+                                           R.layout.widget);
+      widget.setRemoteAdapter(R.id.restaurants,
+                              svcIntent);
+      Intent clickIntent = new Intent(ctxt, EditRestaurant.class);
+      PendingIntent clickPI =
+          PendingIntent
+                       .getActivity(ctxt, 0,
+                                    clickIntent,
+                                    PendingIntent.FLAG_UPDATE_CURRENT);
+      widget.setPendingIntentTemplate(R.id.restaurants, clickPI);
+
+      mgr.updateAppWidget(appWidgetIds[i], widget);
+      super.onUpdate(ctxt, mgr, appWidgetIds);
+    }
   }
 
 }
--- MunchList/src/csci498/strupper/munchlist/WidgetService.java
+++ MunchList/src/csci498/strupper/munchlist/WidgetService.java
@@ -1,60 +1,13 @@
 package csci498.strupper.munchlist;
 
-import android.app.IntentService;
-import android.app.PendingIntent;
-import android.appwidget.AppWidgetManager;
-import android.content.ComponentName;
 import android.content.Intent;
-import android.database.Cursor;
-import android.widget.RemoteViews;
+import android.widget.RemoteViewsService;
 
-public class WidgetService extends IntentService {
-  public WidgetService() {
-    super("WidgetService");
-  }
-
+public class WidgetService extends RemoteViewsService {
   @Override
-  protected void onHandleIntent(Intent intent) {
-    ComponentName me = new ComponentName(this, AppWidget.class);
-    RemoteViews updateViews = new RemoteViews("apt.tutorial",
-                                              R.layout.widget);
-    RestaurantHelper helper = new RestaurantHelper(this);
-    AppWidgetManager mgr = AppWidgetManager.getInstance(this);
-    try {
-      Cursor c = helper
-                       .getReadableDatabase()
-                       .rawQuery("SELECT COUNT(*) FROM restaurant", null);
-      c.moveToFirst();
-      int count = c.getInt(0);
-      c.close();
-      if (count > 0) {
-        int offset = (int)(count * Math.random());
-        String args[] = { String.valueOf(offset) };
-        c =
-            helper
-                  .getReadableDatabase()
-                  .rawQuery("SELECT _ID, name FROM restaurant LIMIT 1 OFFSET ?",
-                            args);
-        c.moveToFirst();
-        updateViews.setTextViewText(R.id.name, c.getString(1));
-        Intent i = new Intent(this, EditRestaurant.class);
-        i.putExtra(MunchList.ID_EXTRA, c.getString(0));
-        PendingIntent pi =
-            PendingIntent.getActivity(this, 0, i,
-                                      PendingIntent.FLAG_UPDATE_CURRENT);
-        updateViews.setOnClickPendingIntent(R.id.name, pi);
-        c.close();
-      }
-      else {
-        updateViews.setTextViewText(R.id.name,
-                                    this.getString(R.string.empty));
-      }
-    } finally {
-      helper.close();
-    }
-    Intent i = new Intent(this, WidgetService.class);
-    PendingIntent pi = PendingIntent.getService(this, 0, i, 0);
-    updateViews.setOnClickPendingIntent(R.id.next, pi);
-    mgr.updateAppWidget(me, updateViews);
+  public RemoteViewsFactory onGetViewFactory(Intent intent) {
+    return new ListViewsFactory(this.getApplicationContext(),
+                                intent);
   }
+
 }
